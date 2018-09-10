7c3f59d662e615f31bb8d42f9b162e35a78c977b
Bernard Angga Sihotang <42725315+BernardAngga@users.noreply.github.com>
2018-09-10 02:33:00 -0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,40 @@
 <?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+ function rekursif2($j, $kol) {
+    global $i;
+    if ($j > $i - $kol) {
+        echo "*";
+        rekursif2($j - 1, $kol);
+    }
 }
+function rekursif($kol){
+  global $i;
+    rekursif2($i, $kol);
+    echo "<br>";
+    if (--$kol > $i){
+        rekursif($kol);
+    }
+}
+$i = 0;
 rekursif(5);
+echo "/*====================================================================================*/<br>";
+function rekursive2($a, $b, $c) {
+    global $cmn;
+    if ($a>$cmn - $b - 1) {
+        echo "$c ";
+        rekursive2($a - 1, $b, $c + 1);
+    }
+}
+function rekursive($b, $g) {
+	global $cmn;
+    rekursive2($cmn, $b, $g);
+    echo "<br>";
+    if (++$b<$cmn){
+        rekursive($b, $g);
+    }
+}
+
+$cmn = 5;
+
+rekursive(0,1);
+
 ?>
